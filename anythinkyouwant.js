1e20486a4ce102775050a84e207db13e4c32c5cc
Maya Wright <mayagwright20@hotmail.com>
2023-02-01 10:49:45 +0000

    your msg

---
+++ anythinkyouwant.js
@@ -0,0 +1,13 @@
+const cameron = 5;
+const maya = 2;
+const vicky = 7;
+const sus = 8;
+const amber = 10; 
+
+function calculateClassScores(){
+    return console.log(cameron + maya + vicky + sus + amber);
+}
+
+calculateClassScores();
+
+
