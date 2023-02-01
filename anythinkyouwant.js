3ab2231f45ece31b71f354a539ad74ebe03fb212
Maya Wright <mayagwright20@hotmail.com>
2023-02-01 16:45:28 +0000

    added learn.js file

--- anythinkyouwant.js
+++ anythinkyouwant.js
@@ -1,13 +1,71 @@
-const cameron = 5;
-const maya = 2;
-const vicky = 7;
-const sus = 8;
-const amber = 10; 
+// const cameron = 5;
+// const maya = 2;
+// const vicky = 7;
+// const sus = 8;
+// const amber = 10; 
 
+
+
+// const arr = [cameron, "cameron", maya, "maya", vicky, "vicky", sus, "sus", amber, "amber"];
+
+// for (let i=0; i<arr.length; i++){
+
+//     if(arr[i] >=6){
+//         console.log(arr[i +1] , " is more than 6");
+//     }else{
+//         console.log( "void");
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function calculateClassScores(){
     return console.log(cameron + maya + vicky + sus + amber);
 }
 
 calculateClassScores();
+
+function HigherThan6(){
+    if(cameron >= 6){
+        console.log("Cameron has a score of 6 or higher");
+    }else{
+        console.log("cameron has a lower score");
+    }
+}
+
+HigherThan6();
+
+
+const height = 50;
+
+function MoreThan40(){
+    if(height<= 40){
+    console.log("Height is less than 40");
+}else{
+    console.log("Height is more than 40");
+}
+}
+
+MoreThan40();
+*/
+
+
 
 
---
+++ learn.js
@@ -0,0 +1,19 @@
+/*const c = "cat";
+let x = 1;
+var a = "ant";
+*/
+
+
+const firstNumber = 5;
+
+function MyNumberFunction (){
+    
+    if(firstNumber == 5 ){
+        console.log ("This is 5");
+    }else{
+        console.log ("This is Not 5");
+    };
+
+};
+
+MyNumberFunction ();
